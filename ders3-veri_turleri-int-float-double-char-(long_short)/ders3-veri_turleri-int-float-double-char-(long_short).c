0305b8935e984bb66d78ce610de4110ec1287d6e
Yusuf Eren KILIÇ <yusuferen.kilic947@gmail.com>
2022-04-14 14:13:33 +0300

    main

---
+++ ders3-veri_turleri-int-float-double-char-(long_short)/ders3-veri_turleri-int-float-double-char-(long_short).c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+
+    /*
+    int a = 15; //virgüllü değerleri tutamaz, tam sayı olarak çıktı verir. maksimum aralığı -+2M kadardır.
+    printf("%d" , a);
+    */
+
+   /*
+   float b = 15.15;
+   printf("%f" , b);
+    */
+
+   /*
+   double c = 15.15;
+   printf("%lf" , c);
+    */
+
+    //printf("%d" , sizeof(double)); // değişkenleri kaç bite yer kapladığınıa böyle bakılabilir
+
+    /*
+    char isim[10] = "yusuf"; //köşeli parantez içinde 10 dizin oluşturduğu için, her biri 1 bite'tan 10 bite yer kaplar.
+    printf("%d" , sizeof(isim));
+    */
+
+    /*
+    unsigned int d = -100; //0 - 4M _ Negatif değerler mevcut değil
+    printf("%d" , d);
+    */
+
+    /*
+    char e = 'e';
+    printf("%c" , e);
+    char f[10] = "faruk";
+    printf("%s" , f);
+    */
+
+    //long long , long int, long double  --  shor int , short double ...
+    
+    getch();
+    return 0;
+}
